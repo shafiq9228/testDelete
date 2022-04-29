5f3aea043f28a23822e5c9d7536fa48004044761
shafiqpatel <shafiqpatel@gmail.com>
2022-04-29 16:00:32 +0530

    test3

--- services/ashafiq.php
+++ services/ashafiq.php
@@ -1,6 +1,6 @@
 <?php
 echo "1";
-$url = "https://apibankingone.icicibank.com/api/v1/composite-payment";   
+$url = "https://www.javatpoint.com/";   
 $ch = curl_init();   
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);   
 curl_setopt($ch, CURLOPT_URL, $url);   
