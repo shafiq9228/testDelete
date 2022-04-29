e941b19a7a2f8c36fb4e316f9eb843100373d90d
shafiqpatel <shafiqpatel@gmail.com>
2022-04-29 16:05:26 +0530

    test3

--- services/ashafiq.php
+++ services/ashafiq.php
@@ -1,10 +1,22 @@
 <?php
-echo "1";
-$url = "https://www.javatpoint.com/";   
-$ch = curl_init();   
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);   
-curl_setopt($ch, CURLOPT_URL, $url);   
-$res = curl_exec($ch);   
-echo $res;  
-
+ 
+      $data = array(
+                "query1" => "parameter1 ",
+                "text" => "Hi, Message from android example"
+              );
+ 
+       $url = "https://androidtraininginstitute.com/dakshinpay/api/verifyotp/";
+ 
+       $ch = curl_init ($url);
+       curl_setopt ($ch, CURLOPT_POST, true);
+       curl_setopt ($ch, CURLOPT_POSTFIELDS, $data);
+       curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true);
+ 
+       $response = curl_exec ($ch);
+       if(!response){
+               die("Error: "" . curl_error($ch) . "" - Code: " . curl_errno($ch));
+         }
+ 
+       curl_close($crl);
+ 
 ?>
